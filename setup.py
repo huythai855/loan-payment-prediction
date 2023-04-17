8c470d95ac60ebe539944643249c4b5df44093ca
huythai855 <nguyenhuythai15@gmail.com>
2023-04-18 00:32:24 +0700

    Initialize the repo with Cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A simple model to predict the user's ability to pay the loan.',
+    author='huythai855',
+    license='',
+)
